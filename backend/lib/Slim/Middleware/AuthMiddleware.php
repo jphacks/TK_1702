a2473f0e46af424e8ca5354aba30a124fbd5a05b
Soma Ueda <sora@turenar.xyz>
2017-10-29 00:21:54 +0900

    add x-udid checker

---
+++ backend/lib/Slim/Middleware/AuthMiddleware.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types=1);
+
+namespace SlashApp\Slim\Middleware;
+
+
+use ORM\User;
+use ORM\UserQuery;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use SlashApp\JsonRenderer;
+use Slim\Interfaces\RouteInterface;
+
+class AuthMiddleware
+{
+	const ROUTE_ARG_SKIP = 'auth.skip';
+
+	public function __invoke(
+		ServerRequestInterface $request,
+		ResponseInterface $response,
+		callable $next
+	): ResponseInterface {
+		$udid = $request->getHeaderLine('X-UDID');
+		$route = $this->getRoute($request);
+		$auth_skipped = $route && $route->getArgument(self::ROUTE_ARG_SKIP);
+
+		if (!$auth_skipped) {
+			if (empty($udid)) {
+				return JsonRenderer::create()->renderAsError($response, 'empty udid', 403);
+			}
+			$user = transaction(function () use ($udid) {
+				$user = UserQuery::create()
+					->filterByUdid($udid)
+					->findOne();
+				if ($user === null) {
+					$user = new User();
+					$user
+						->setUdid($udid)
+						->save();
+				}
+				return $user;
+			});
+			$request = $request->withAttribute('user', $user);
+		}
+
+		return $next($request, $response);
+	}
+
+	public function getRoute(ServerRequestInterface $request):?RouteInterface
+	{
+		return $request->getAttribute('route');
+	}
+}
