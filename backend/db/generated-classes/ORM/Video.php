73dc71e7c7fd9077de525b37f4691f23514db28c
totetotetotem <kyarioka7945@gmail.com>
2017-10-29 03:49:48 +0900

    video upload

---
+++ backend/db/generated-classes/ORM/Video.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace ORM;
+
+use ORM\Base\Video as BaseVideo;
+
+/**
+ * Skeleton subclass for representing a row from the 'video' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class Video extends BaseVideo
+{
+
+}
