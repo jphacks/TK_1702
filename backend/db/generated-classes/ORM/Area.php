1866aecd88505c32258c2f59658d9f76bb97db80
Soma Ueda <sora@turenar.xyz>
2017-10-28 16:45:16 +0900

    update db schema

---
+++ backend/db/generated-classes/ORM/Area.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace ORM;
+
+use ORM\Base\Area as BaseArea;
+
+/**
+ * Skeleton subclass for representing a row from the 'area' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class Area extends BaseArea
+{
+
+}
