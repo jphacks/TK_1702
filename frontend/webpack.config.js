9fe29047e026b689068e15e46b2236d6f81d1cc0
Hiroaki KARASAWA <siqvare@gmail.com>
2017-10-28 10:55:25 +0900

    Set up webpack

---
+++ frontend/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('../backend/public'),
+    filename: 'index.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  }
+}
