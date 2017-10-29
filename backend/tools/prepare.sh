d45eb997f290442df7e62f039f8e5cbf482ddc88
Soma Ueda <sora@turenar.xyz>
2017-10-29 10:10:45 +0900

    chmod

--- backend/tools/prepare.sh
+++ backend/tools/prepare.sh
@@ -5,6 +5,8 @@ source tools/common.sh
 
 test -d build/generated-classes || exec_cmd mkdir -p build/generated-classes
 
+exec_cmd chmod 777 static/thumb/ static/video/
+
 exec_cmd sed -e "s/{{env}}/${CHAT_ENV}/" config/env.php.in >config/env.php
 exec_cmd php composer.phar install $(env_if dev '' '--no-dev')
 exec_cmd vendor/bin/propel model:build
