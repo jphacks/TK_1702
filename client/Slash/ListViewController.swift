5cde32a712ea90b88262f15ee77f4f8470075484
Akari Asai <akari.1311aquamarine4@gmail.com>
2017-11-10 19:41:21 +0900

    Add an alertView

--- client/Slash/ListViewController.swift
+++ client/Slash/ListViewController.swift
@@ -12,6 +12,7 @@ import Foundation
 class ListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var lineButton: UIButton!
     
     var placeList = ["本郷1丁目","本郷2丁目", "本郷3丁目","本郷4丁目","本郷5丁目","本郷6丁目","本郷7丁目", "本郷8丁目"]
     var timeList = ["2017年10月22日","2017年10月23日", "2017年10月24日","2017年10月27日","2017年10月27日","2016年9月17日","2016年11月1日", "2017年12月31日"]
@@ -72,5 +73,21 @@ class ListViewController: UIViewController, UITableViewDataSource, UITableViewDe
     //データの個数を返すメソッド
     func tableView(_ tableView:UITableView, numberOfRowsInSection section:Int) -> Int {
         return videos.count
+    }
+    @IBAction func pushLineButton(_ sender: Any) {
+        let alertController = UIAlertController(title: "Line連携します", message: "totem", preferredStyle: .alert)
+        
+        let otherAction = UIAlertAction(title: "はい", style: .default) {
+            action in NSLog("はいボタンが押されました")
+        }
+        let cancelAction = UIAlertAction(title: "いいえ", style: .cancel) {
+            action in NSLog("いいえボタンが押されました")
+        }
+        
+        // addActionした順に左から右にボタンが配置されます
+        alertController.addAction(otherAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
     }
 }
